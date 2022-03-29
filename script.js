b683b3c59c71df6f89cc385138d1f7895532f122
Nisha Brijesh <92055350+nbhawnani@users.noreply.github.com>
2022-03-29 13:52:09 +0000

    arrow functions

--- script.js
+++ script.js
@@ -8,16 +8,45 @@ Sample code to be commented out
 
 //ShowName("Hiya! ","Nisha", "Brijesh");
 
+// Function to concatenate 2 values accepted as parameters:
+/*
 function join2Strings(message, value){
   console.log(message + " " + value );
+  
 }
 
-/*
 let msg=''
 let val=''
 msg=prompt('Enter your message:');
 val=prompt('Enter your value:');
+
+join2Strings(msg,val);
 */
-join2Strings('test Message','test value');
 
+// Invoking a function via a variable. using return command in the function.
 
+function getMyNumber(number) {
+return "718" + number;
+}
+
+/*
+let myNumberWithAreaCode = getMyNumber("567-9000");
+console.log(myNumberWithAreaCode);
+*/
+
+//console.log(typeof(getMyNumber)); // function
+
+/*
+// Arrow functions:
+// setInterval and clearInterval commands :
+let counter = 5
+let countDown = setInterval(() => {
+console.log(counter)
+counter-- //counter=counter-1
+if (counter === 0) {
+console.log("I have no name!!")
+clearInterval(countDown)
+}
+}, 1000) // interval is specified in milliseconds
+
+*/
