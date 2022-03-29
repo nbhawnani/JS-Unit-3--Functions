cd2745ce99139984cf3ca6c1091f126f13be6d83
Nisha Brijesh <92055350+nbhawnani@users.noreply.github.com>
2022-03-29 13:01:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function ShowName(greeting, first, last) {
+alert(greeting + " " + first + " " + last);
+//console.log(greeting + " " + first + " " + last);
+  /*
+Sample code to be commented out
+  */
+}
+
+//ShowName("Hiya! ","Nisha", "Brijesh");
+
+function join2Strings(message, value){
+  console.log(message + " " + value );
+}
+
+/*
+let msg=''
+let val=''
+msg=prompt('Enter your message:');
+val=prompt('Enter your value:');
+*/
+join2Strings('test Message','test value');
+
+
