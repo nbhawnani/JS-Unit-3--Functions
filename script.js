5097e06b3d502aaba999123d3fba080d326ddb58
Nisha Brijesh <92055350+nbhawnani@users.noreply.github.com>
2022-10-18 19:09:47 +0000

    ..

--- script.js
+++ script.js
@@ -15,17 +15,18 @@ function Add2Num(num1, num2) {
 
 //Invoking the function with arguments
 let answer=Add2Num(1,2);
-console.log("The sum is:",answer)
+console.log("The sum is:",answer);
 
 console.log(typeof(Add2Num)); // function
 
 //Declaring a function with no parameters
 function Hello(){
-  console.log("Hi Class! how are you doing today?")
+  console.log("Hi Class! how are you doing today?");
 }
 
 Hello();
 
+/*
 //sending arguments based on user response
 //Function declaration
 function sayHello(firstlastname) {
@@ -35,3 +36,47 @@ alert("Hello " + firstlastname);
 let strName=prompt("Enter your name:")
 //Invoking the function with arguments
 sayHello(strName);
+*/
+
+
+//Arrow functions - with no return 
+/*
+let sayHello2 = (firstlastname) => {
+  alert("Hello " + firstlastname);
+}
+
+let strName2=prompt("Enter your name:")
+//Invoking the function with arguments
+sayHello2(strName2);
+*/
+
+
+//Arrow function declaration using return statement
+let product2Num = (num1, num2) => num1*num2;
+
+//Invoking the arrow function with arguments
+let prod=product2Num(10,21);
+console.log("The product is:",prod)
+
+//line of code = 1 instruction in javascript that ends with a semi-colon;
+
+
+//setTimeout
+//Original method of declaring function
+function Happy(){
+  alert("Happy Birthday!!");
+} 
+
+let intTimeoutID = setTimeout(Happy,3000)
+clearTimeout(intTimeoutID)
+
+
+//setInterval method
+let counter=10
+let countdown = () => {
+  console.log(counter);
+  counter--;
+} 
+
+let intTimeoutID2=setInterval(countdown,1000)
+clearInterval(intTimeoutID2)
--- script2.js
+++ script2.js
@@ -2,36 +2,11 @@
 
 
 
-/*
-// Arrow functions:
-// setInterval and clearInterval commands :
-let counter = 5
-let countDown = setInterval(() => {
-console.log(counter)
-counter-- //counter=counter-1
-if (counter === 0) {
-console.log("I have no name!!")
-clearInterval(countDown)
-}
-}, 1000) // interval is specified in milliseconds
-
-*/
-
-//Original method of declaring function
-let Happy = function(){
-  alert("Happy Birthday!!");
-} 
-
-//Arrow function
-//let Happy = () => alert("Happy Birthday!!");
 
 
-let intTimeoutID=setTimeout(Happy,3000)
 
-let counter=10
-let countdown = function(){
-  console.log(counter);
-  counter--;
-} 
 
-let intTimeoutID2=setInterval(countdown,1000)
+
+
+
+
